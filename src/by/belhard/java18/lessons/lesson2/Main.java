414448ed7a5af6f35263997d03bf8984d4340c10
create-delete <pomah100@gmail.com>
2019-10-15 21:16:44 +0300

    after second lesson

---
+++ src/by/belhard/java18/lessons/lesson2/Main.java
@@ -0,0 +1,11 @@
+package by.belhard.java18.lessons.lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+    }
+}
