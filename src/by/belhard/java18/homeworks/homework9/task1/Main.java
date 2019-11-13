091cb32a19691f133d0889f45356d083bf7e25ff
create-delete <pomah10)@gmail.com>
2019-11-13 16:58:07 +0300

    first task

---
+++ src/by/belhard/java18/homeworks/homework9/task1/Main.java
@@ -0,0 +1,25 @@
+package by.belhard.java18.homeworks.homework9.task1;
+
+//создать файл с числами, разделенными пробелами. Записать в другой файл кубы этих чисел
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("input.ti");
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.ti"));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("output.to"))
+        ) {
+            String[] splitted = reader.readLine().trim().split("\\s+");
+
+            for (int i = 0; i < splitted.length; i++) {
+                int number = Integer.parseInt(splitted[i]);
+
+                writer.write((number*number*number) + " ");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
